873ee82697aeff42ea34c4dd77ec80b3fce9d780
lisban-browserstack <lisban@browserstack.com>
2024-06-30 23:06:40 +0530

    first commit

---
+++ android/examples/run-sample-test/test.conf.js
@@ -0,0 +1,69 @@
+exports.config = {
+  user: process.env.BROWSERSTACK_USERNAME || '',
+  key: process.env.BROWSERSTACK_ACCESS_KEY || '', 
+
+  hostname: 'hub.browserstack.com',
+
+  services: [
+    [
+      'browserstack',
+      {
+        buildIdentifier: '${BUILD_NUMBER}',
+        browserstackLocal: true,
+        opts: { forcelocal: false, localIdentifier: "webdriverio-appium-app-browserstack-repo" },
+        app: process.env.BROWSERSTACK_APP_PATH || 'bs://5674acb904bba83eb33414bd9afaa79645b5b5ba',
+      }
+    ]
+  ],
+
+  capabilities: [{
+    'bstack:options': {
+      deviceName: 'Google Pixel 3',
+      osVersion: "9.0"
+    }
+  }, {
+    'bstack:options': {
+      deviceName: 'Samsung Galaxy S10e',
+      osVersion: "9.0"
+    }
+  }],
+
+  commonCapabilities: {
+    'bstack:options': {
+      projectName: "BrowserStack Samples",
+      buildName: 'browserstack build',
+      sessionName: 'BStack parallel webdriverio-appium',
+      debug: true,
+      networkLogs: true,
+      source: 'webdriverio:appium-sample-sdk:v1.0'
+    }
+  },
+
+  maxInstances: 10,
+
+  updateJob: false,
+  specs: [
+    './examples/run-sample-test/specs/single_test.js'
+  ],
+  exclude: [],
+
+  logLevel: 'info',
+  coloredLogs: true,
+  screenshotPath: './errorShots/',
+  baseUrl: '',
+  waitforTimeout: 10000,
+  connectionRetryTimeout: 90000,
+  connectionRetryCount: 3,
+
+  framework: 'mocha',
+  mochaOpts: {
+    ui: 'bdd',
+    timeout: 20000
+  }
+};
+
+// Code to support common capabilities
+exports.config.capabilities.forEach(function(caps){
+  for(let key in exports.config.commonCapabilities) 
+    caps[key] = { ...caps[key], ...exports.config.commonCapabilities[key]};
+});
